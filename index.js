96d306a3125a7a59c0b30187412382359459ac41
Blaire <grant.blaire@gmail.com>
2021-06-03 20:01:37 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// result will equal 62
+let num1 = 1;
+let num2 = 62;
+let multiply = num1 * num2;
 
-// Write your code here
+// generate a random integer greater than 0
+let random = Math.ceil(Math.random() * 10 + 1);
+
+// an equation whose remainder is equal to 4
+let num3 = 11;
+let num4 = 15;
+let mod = num4 % num3;
+
+// return 20 as the highest number in the set
+let max = Math.max(1, 3, 5, 16, 20);
